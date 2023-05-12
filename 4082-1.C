bf597358f6305a8b44287da32c0f3c330606fc58
mynklathiya <132878852+mynklathiya@users.noreply.github.com>
2023-05-12 13:04:24 +0530

    Add files via upload

---
+++ 4082-1.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+   int p=500,q=600,r=700;
+   clrscr();
+   if(p>q)
+   {
+      printf("p is minimum");
+   }
+   else
+   {
+      printf("r is minimum");
+   }
+  }
+  else
+  {
+  if(q<r)
+  {
+     printf("q is minimum");
+  }
+  else
+  {
+     printf("r is minimum");
+  }
+ }
+ getch();
+}
+
+
+
+
+
+
+
+  }
+
+
+
+
